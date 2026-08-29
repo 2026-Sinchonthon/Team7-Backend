ebcd0a2def60177930317161e046bce55a128689
chae_one <chaewonp2005@gmail.com>
2026-08-29 16:22:55 +0900

    feat: 지정대학교5 기능추가

--- src/main/java/sinchonthon/demo/domain/member/dto/StudentSignupRequest.java
+++ src/main/java/sinchonthon/demo/domain/member/dto/StudentSignupRequest.java
@@ -1,6 +1,7 @@
 package sinchonthon.demo.domain.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,10 @@ import lombok.NoArgsConstructor;
 public class StudentSignupRequest {
 
     @NotBlank(message = "대학교는 필수입니다.")
+    @Pattern(
+            regexp = "연세대학교|이화여자대학교|서강대학교|홍익대학교|명지대학교",
+            message = "대학교는 연세대학교, 이화여자대학교, 서강대학교, 홍익대학교, 명지대학교 중 하나여야 합니다."
+    )
     private String university;
 
     @NotBlank(message = "닉네임은 필수입니다.")
